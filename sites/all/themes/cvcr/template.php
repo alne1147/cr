fb763228b3a3eb7ed285916c37d1bbe855e46270
anevarez <alfredo.nevarez@www.colorado.gov>
2016-03-08 13:51:02 -0700

    adding classes to link items. Gulp is for macbook air now.

--- sites/all/themes/cvcr/template.php
+++ sites/all/themes/cvcr/template.php
@@ -17,3 +17,17 @@ function cvcr_preprocess_page(&$variables) {
  */
 function cvcr_preprocess_node(&$variables) {
 }
+
+/**
+* theme_menu_link()
+*/
+function cvcr_menu_link(array $variables) {
+//add class for li
+   $variables['element']['#attributes']['class'][] = 'menu-' . $variables['element']['#original_link']['mlid'];
+//add class for a
+   $variables['element']['#localized_options']['attributes']['class'][] = 'menu-' . $variables['element']['#original_link']['mlid'];
+//dvm($variables['element']);
+  return theme_menu_link($variables);
+}
+?>
+
